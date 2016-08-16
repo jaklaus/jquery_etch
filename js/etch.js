d1ce09c999c2277cb02b50a7dc8bd75ac820a1c8
Joel Klaus <jaklaus0101@gmail.com>
2016-08-16 16:20:48 -0500

    add comments

--- js/etch.js
+++ js/etch.js
@@ -1,46 +1,59 @@
-createSquares(9);
-colorHover();
-resetSquares();
-squaresNumber();
 
+// set default grid size
+var gridSize = 9;
 
+// call functions and create default grid
+$(document).ready(function(){
+	createSquares(gridSize);
+	colorHover();
+	resetSquares();
+	setGridSize();
+	gridSizeDisplay(gridSize);
+})
 
-
+// creates rows of squares. assigns class 'squares', width, and padding to each square. uses nested for loop to draw columns
 function createSquares(value){
 	var size = 600 / value;
 	var container = $("#container").html("");
 	for(var i = 1; i <= value; i++){
-		
 		for(var j = 1; j <= value; j++){
 			container.append($("<div></div>").addClass('squares').css({'width': size + "px", 'padding-bottom': size + "px"}));
 		}
-		container.append($("<div></div>").css({"clear": "both"}));
+			container.append($("<div></div>").css({"clear": "both"}));
 	}
-	
 }
 
+// hover functionality created through adding squares-hover class
 function colorHover(){
 	$('.squares').hover(function(){
 		$(this).addClass('squares-hover');
 	})
 }
 
+// clicking button resets grid to its default state
 function resetSquares(){
 	$('#reset-squares').click(function(){
 		$('.squares').remove();
-		createSquares(9);
+		createSquares(gridSize);
 		colorHover();
+		gridSizeDisplay(gridSize);
 	})
 }
 
-function squaresNumber(){
+// with input and button set new grid size.
+function setGridSize(){
 	$('#squares-number-button').click(function(){
-		var gridNumber = $('#squares-number-input').val();
-		if(gridNumber > 0){
+		var gridSize = $('#squares-number-input').val();
+		if(gridSize > 0){
 			$('.squares').remove();
-			createSquares(gridNumber);
+			createSquares(gridSize);
 			colorHover();
+			gridSizeDisplay(gridSize);
 		}
 		
 	})
+}
+// displays current grid size on page
+function gridSizeDisplay(value){
+	$('#grid-size').html("<p>Grid Size: </p>" + value)
 }
